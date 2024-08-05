11d6ab2639526e3d15c77491ff93add030030a67
JJ Wong <jjwong0915@gmail.com>
2024-08-05 15:49:24 -0400

    refactor: remove context from controller

--- src/BlogAPI/Program.cs
+++ src/BlogAPI/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using BlogAPI.Models;
+using BlogAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BlogContext>(opt => opt.UseInMemoryDatabase("BlogPlatform"));
+builder.Services.AddScoped<IUserRepository>(sp => new UserRepository(sp.GetRequiredService<BlogContext>()));
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
