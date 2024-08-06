8a8a066fad01a4ea40843679c81201e6b5035217
Daniel <dbellonzi@aol.com>
2024-08-05 22:00:50 -0700

    updated context

--- src/BlogAPI/Models/BlogContext.cs
+++ src/BlogAPI/Models/BlogContext.cs
@@ -6,4 +6,8 @@ namespace BlogApi.Models;
 public class BlogContext(DbContextOptions<BlogContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Tag> Tags { get; set; }
 }
